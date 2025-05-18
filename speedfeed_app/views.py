af08dc46191524ce2deb67379dc0ea9776359917
Your Name <you@example.com>
2025-05-18 09:35:04 +0000

    Update some UIs to add auth buttons

--- speedfeed_app/views.py
+++ speedfeed_app/views.py
@@ -2,11 +2,12 @@
 Views for the SpeedFeed app.
 """
 
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import CreateView, DeleteView, DetailView, ListView, UpdateView
 from django.urls import reverse_lazy
 from .models import Restaurant
 
-class RestaurantCreateView(CreateView):
+class RestaurantCreateView(LoginRequiredMixin, CreateView):
     """
     A restaurant create view.
     """
@@ -20,27 +21,30 @@ class RestaurantCreateView(CreateView):
             field.field.widget.attrs.update({'class' : 'form-control offset-1'})
         return form
 
-class RestaurantDeleteView(DeleteView):
+class RestaurantDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     """
     A restaurant delete view.
     """
     model = Restaurant
     success_url = reverse_lazy('speedfeed_app:restaurant_list')
 
-class RestaurantDetailView(DetailView):
+    def test_func(self):
+        return self.request.user.is_staff
+
+class RestaurantDetailView(LoginRequiredMixin, DetailView):
     """
     A restaurant detail view.
     """
     model = Restaurant
 
-class RestaurantListView(ListView):
+class RestaurantListView(LoginRequiredMixin, ListView):
     """
     A restaurant list view.
     """
     model = Restaurant
     paginate_by = 20
 
-class RestaurantUpdateView(UpdateView):
+class RestaurantUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     """
     A restaurant update view.
     """
@@ -53,3 +57,6 @@ class RestaurantUpdateView(UpdateView):
         for field in form:
             field.field.widget.attrs.update({'class' : 'form-control offset-1'})
         return form
+
+    def test_func(self):
+        return self.request.user.is_staff
