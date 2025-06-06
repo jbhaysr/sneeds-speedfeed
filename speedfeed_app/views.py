c55d276ac324ee6a0c771932bab39faff8033bc0
Your Name <you@example.com>
2025-06-06 07:28:48 +0000

    Reference address through to location in API spec

--- speedfeed_app/views.py
+++ speedfeed_app/views.py
@@ -5,7 +5,7 @@ Views for the SpeedFeed app.
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import CreateView, DeleteView, DetailView, ListView, UpdateView
 from django.urls import reverse_lazy
-from rest_framework import viewsets
+from rest_framework import permissions, viewsets
 from .models import Restaurant
 from .permissions import IsStaffOrReadOnly
 from .serializers import RestaurantSerializer
@@ -67,7 +67,7 @@ class RestaurantUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
 class RestaurantViewSet(viewsets.ModelViewSet):
     queryset = Restaurant.objects.all().order_by('-id')
     serializer_class = RestaurantSerializer
-    permission_classes = [IsStaffOrReadOnly]
-    filterset_fields = ['name', 'address', 'currency']
-    search_fields = ['name', 'address', 'currency']
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsStaffOrReadOnly]
+    filterset_fields = ['name', 'address__location', 'currency']
+    search_fields = ['name', 'address__location', 'currency']
     ordering_fields = ['rating', 'name']
